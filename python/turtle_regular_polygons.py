8e3fae113318acd46a1707dafa08956aca6df579
BlueBloxKraft <94107880+BlueBloxKraft@users.noreply.github.com>
2023-10-28 23:50:51 -0700

    Update and rename turtle_polygons.py to turtle_regular_polygons.py

--- python/turtle_regular_polygons.py
+++ python/turtle_regular_polygons.py
@@ -2,7 +2,7 @@ from turtle import *
 
 def drawRegPolygon(noOfSides, sideLength):
     if(noOfSides>2):
-        for i in range(0, noOfSides):
+        for i in range(noOfSides):
             forward(sideLength)
             left(360/noOfSides)
     else:
